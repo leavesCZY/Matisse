b1c28558d45cd0c110c49011ca0c79774c9b3360
vicky leu <yu6564172@gmail.com>
2023-06-15 19:07:42 +0800

    kts jitpack maven config

--- matisse/build.gradle.kts
+++ matisse/build.gradle.kts
@@ -4,8 +4,38 @@ plugins {
     id("com.android.library")
     kotlin("android")
     id("org.jetbrains.kotlin.plugin.parcelize")
+    `maven-publish`
 }
-val composeCompilerVersion:String by properties
+description = "Matisse for Jetpack Compose"
+group = "github.leavesczy"
+version = "1.0.0"
+
+// 配置发布到本地 Maven 仓库
+afterEvaluate {
+    publishing {
+        repositories {
+            mavenLocal()
+        }
+        // 将 'matisse' 组件发布到本地 Maven 仓库
+        publications {
+            // 定义Maven发布以便将模块发布到本地或远程 Maven 存储库。
+            create<MavenPublication>("release") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components["release"])
+                pom {
+                    packaging = "jar"
+                    description.set(project.description)
+                    name.set(project.name)
+                }
+            }
+        }
+    }
+}
+
+
+val composeCompilerVersion: String by properties
 android {
     namespace = "github.leavesczy.matisse"
     compileSdk = 33
@@ -35,20 +65,20 @@ android {
 }
 
 dependencies {
-    val composeUIVersion:String by properties
-    val junitVersion:String by properties
-    val junitExtVersion:String by properties
-    val espressoVersion:String by properties
-    val appcompatVersion:String by properties
-    val activityComposeVersion:String by properties
-    val composeBomVersion:String by properties
-    val composeMaterial3Version:String by properties
+    val composeUIVersion: String by properties
+    val junitVersion: String by properties
+    val junitExtVersion: String by properties
+    val espressoVersion: String by properties
+    val appcompatVersion: String by properties
+    val activityComposeVersion: String by properties
+    val composeBomVersion: String by properties
+    val composeMaterial3Version: String by properties
 
     testImplementation("junit:junit:$junitVersion")
     androidTestImplementation("androidx.test.ext:junit:$junitExtVersion")
     androidTestImplementation("androidx.test.espresso:espresso-core:$espressoVersion")
     implementation("androidx.appcompat:appcompat:$appcompatVersion")
-    implementation("androidx.activity:activity-compose:activityComposeVersion")
+    implementation("androidx.activity:activity-compose:$activityComposeVersion")
     implementation(platform("androidx.compose:compose-bom:$composeBomVersion"))
     implementation("androidx.compose.material3:material3:$composeMaterial3Version")
     implementation("androidx.compose.material:material-icons-extended:$composeUIVersion")
