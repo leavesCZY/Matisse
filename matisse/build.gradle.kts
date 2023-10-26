de8facfb94746f370d95818a9ccd4d5b3a43a124
leavesCZY <1990724437@qq.com>
2023-10-26 15:25:38 +0800

    调整 maven-publish 脚本

--- matisse/build.gradle.kts
+++ matisse/build.gradle.kts
@@ -41,54 +41,78 @@ android {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "io.github.leavesczy"
-            artifactId = "matisse"
-            version = "0.0.1-test03"
-            afterEvaluate {
-                from(components["release"])
-            }
-            pom {
-                name = "Matisse"
-                description = "A Image/Video Selector Implemented with Jetpack Compose"
-                url = "https://github.com/leavesCZY/Matisse"
-                licenses {
-                    license {
-                        name = "The Apache License, Version 2.0"
-                        url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                        distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "leavesCZY"
-                        name = "leavesCZY"
-                        email = properties.getting("ossrh.email")
-                    }
-                }
-                scm {
-                    url = "https://github.com/leavesCZY/Matisse"
-                    connection = "https://github.com/leavesCZY/Matisse"
-                    developerConnection = "https://github.com/leavesCZY/Matisse"
-                }
-            }
-        }
-    }
-    repositories {
-        maven {
-            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = properties["ossrh.username"].toString()
-                password = properties["ossrh.password"].toString()
-            }
-        }
-    }
-}
+val signingKeyId = properties["signing.keyId"]?.toString()
+val signingPassword = properties["signing.password"]?.toString()
+val signingSecretKeyRingFile = properties["signing.secretKeyRingFile"]?.toString()
+val mavenCentralUserName = properties["mavenCentral.username"]?.toString()
+val mavenCentralEmail = properties["mavenCentral.email"]?.toString()
+val mavenCentralPassword = properties["mavenCentral.password"]?.toString()
 
-signing {
-    sign(publishing.publications["release"])
+if (signingKeyId != null
+    && signingPassword != null
+    && signingSecretKeyRingFile != null
+    && mavenCentralUserName != null
+    && mavenCentralEmail != null
+    && mavenCentralPassword != null
+) {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "io.github.leavesczy"
+                artifactId = "matisse"
+                version = "1.1.1"
+                afterEvaluate {
+                    from(components["release"])
+                }
+                pom {
+                    name = "Matisse"
+                    description = "A Image/Video Selector Implemented with Jetpack Compose"
+                    url = "https://github.com/leavesCZY/Matisse"
+                    licenses {
+                        license {
+                            name = "The Apache License, Version 2.0"
+                            url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                            distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                        }
+                    }
+                    developers {
+                        developer {
+                            id = "leavesCZY"
+                            name = "leavesCZY"
+                            email = mavenCentralEmail
+                        }
+                    }
+                    scm {
+                        url = "https://github.com/leavesCZY/Matisse"
+                        connection = "https://github.com/leavesCZY/Matisse"
+                        developerConnection = "https://github.com/leavesCZY/Matisse"
+                    }
+                }
+            }
+        }
+        repositories {
+            maven {
+                setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = mavenCentralUserName
+                    password = mavenCentralPassword
+                }
+            }
+        }
+    }
+    signing {
+        sign(publishing.publications["release"])
+    }
+} else {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                afterEvaluate {
+                    from(components["release"])
+                }
+            }
+        }
+    }
 }
 
 dependencies {
