1391dfdf0c512e3090393de9aede3fa1dcca08d0
一式 <yishi@rhinox.cn>
2024-06-20 09:51:25 +0800

    Initial commit

--- matisse/build.gradle.kts
+++ matisse/build.gradle.kts
@@ -27,75 +27,32 @@ dependencies {
     compileOnly(libs.coil.compose)
 }
 
-val signingKeyId = properties["signing.keyId"]?.toString()
-val signingPassword = properties["signing.password"]?.toString()
-val signingSecretKeyRingFile = properties["signing.secretKeyRingFile"]?.toString()
-val mavenCentralUserName = properties["mavenCentral.username"]?.toString()
-val mavenCentralEmail = properties["mavenCentral.email"]?.toString()
-val mavenCentralPassword = properties["mavenCentral.password"]?.toString()
-
-if (signingKeyId != null
-    && signingPassword != null
-    && signingSecretKeyRingFile != null
-    && mavenCentralUserName != null
-    && mavenCentralEmail != null
-    && mavenCentralPassword != null
-) {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                groupId = "io.github.leavesczy"
-                artifactId = "matisse"
-                version = libs.versions.matisse.publishing.get()
-                afterEvaluate {
-                    from(components["release"])
-                }
-                pom {
-                    name = "Matisse"
-                    description = "A Image/Video Selector Implemented with Jetpack Compose"
-                    url = "https://github.com/leavesCZY/Matisse"
-                    licenses {
-                        license {
-                            name = "The Apache License, Version 2.0"
-                            url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                            distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                        }
-                    }
-                    developers {
-                        developer {
-                            id = "leavesCZY"
-                            name = "leavesCZY"
-                            email = mavenCentralEmail
-                        }
-                    }
-                    scm {
-                        url = "https://github.com/leavesCZY/Matisse"
-                        connection = "https://github.com/leavesCZY/Matisse"
-                        developerConnection = "https://github.com/leavesCZY/Matisse"
-                    }
-                }
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.yishi"
+            artifactId = "matisse"
+            version = "0.0.1"
+            afterEvaluate {
+                from(components["release"])
             }
         }
-        repositories {
-            maven {
-                setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-                credentials {
-                    username = mavenCentralUserName
-                    password = mavenCentralPassword
-                }
+        create<MavenPublication>("debug") {
+            groupId = "com.yishi"
+            artifactId = "matisse"
+            version = "0.0.1"
+            afterEvaluate {
+                from(components["debug"])
             }
         }
     }
-    signing {
-        sign(publishing.publications["release"])
-    }
-} else {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                afterEvaluate {
-                    from(components["release"])
-                }
+    repositories {
+        maven {
+            isAllowInsecureProtocol = true
+            url = uri("http://192.168.10.234:8081/repository/android-maven-sdk/")
+            credentials {
+                username = "android"
+                password = "android123"
             }
         }
     }
